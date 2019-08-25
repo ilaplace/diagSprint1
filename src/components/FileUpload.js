181327dee02fd79214ea689c02a9791ea41745bd
ilaplace <msemihkilic@gmail.com>
2019-08-25 14:35:33 +0300

    frontend fileuplader is fixed with axios

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,76 +1,38 @@
-import React, {Component, useState} from 'react';
-import {useMutation} from '@apollo/react-hooks';
-import gql from 'graphql-tag';
-const { Mutation } = require('react-apollo')
+import React, { useState} from 'react';
+import axios from 'axios';
 
-const UPLOAD = gql`
-    mutation Upload($file: Upload! ){
-        uploadImage(file: $file)
-            
-        
-    }
-`;
+
 const Uploader = () => {
     const [selectedFile, setFile] = useState(null);
-    const [upload, {data}] = useMutation(UPLOAD);
     const fileInput = React.createRef();
     
+    const sendToServer = async (data)=> {
+        const response = await axios.post("http://localhost:5000",
+                    data, {});
+        console.log(response);
+        
+    };
     const handleSubmit = (e) => {
         e.preventDefault();
-            setFile(fileInput.current.files[0]);
-            upload({variables: {selectedFile}})
-            console.log(selectedFile)
-     
+        const data = new FormData();
+        data.append(selectedFile.name, selectedFile);
+        sendToServer(data);
+        
     };
-
-    
         return (
             <form onSubmit={handleSubmit}>
                 <label>
                     Upload file:
-                    <input type="file" ref={fileInput} />
+                    <input type="file" 
+                            ref={fileInput} 
+                            onChange={(e)=>{setFile(e.target.files[0])}}/>
                 </label>
                 <br />
                 <button type="submit">Submit</button>
-
             </form>
         );
     
     
 }
-class FileUpload extends Component{
-    constructor(props){
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.fileInput = React.createRef();
-        this.state={
-            selectedFile: null
-        }
-        
-    };
 
-    handleSubmit(event){
-        event.preventDefault();
-        if(this.state.selectedFile){
-            const data = new FormData();
-            data.append('file', this.state.selectedFile)
-            //alert(`Selected file - ${this.fileInput.current.files[0].name}`)
-        }else
-        alert(`Selecte a file dumbass`)
-    };
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Upload file:
-                    <input type="file" ref={this.fileInput} />
-                </label>
-                <br />
-                <button type="submit">Submit</button>
-
-            </form>
-        );
-    }
-}
 export default Uploader;
